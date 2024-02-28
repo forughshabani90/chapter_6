0cdc1e9dd7fd9d3fd1c2f5b5da47609101e8bfaa
forughshabani90 <forugh.shabani.90@gmail.com>
2024-02-28 01:16:46 -0800

    first commit

---
+++ src/sumForWhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sumForWhile {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i);
+        }
+        for (int k = 1; k < 10; k++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.println(k * j);
+
+            }
+        }
+
+    }
+}
+
